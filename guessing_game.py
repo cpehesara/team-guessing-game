13f29f8323b7062d2878eb99b008ebaf5b9059b3
Pehesara <pehesaramunassinghe13@gmail.com>
2025-05-03 09:22:01 +0530

    Resolve the conflicts and update the range from 1 - 75

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,8 @@
 import random 
 while True: 
-    number = random.randint(1, 50) 
-    print("Guess a number between 1 and 50") 
+    number = random.randint(1, 75) 
+    #change the range from 1 - 75
+    print("Guess a number between 1 and 75") 
     guess = int(input()) 
     if guess == number: 
         print("You win!") 
