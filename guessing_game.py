ae185003aa97402bf5c5a295098b880733edabaf
Pehesara <pehesaramunassinghe13@gmail.com>
2025-05-03 09:49:05 +0530

    Setup error fixings

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,6 @@
 import random 
 while True: 
     number = random.randint(1, 75) 
-    #change the range from 1 - 75
     print("Guess a number between 1 and 75") 
     guess = int(input()) 
     if guess == number: 
