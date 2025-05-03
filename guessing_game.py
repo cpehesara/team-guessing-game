cc18fc372c49725382e1fcd963a862c0ff136206
Pehesara <pehesaramunassinghe13@gmail.com>
2025-05-03 08:59:28 +0530

    Add hints to handle exceptions

--- guessing_game.py
+++ guessing_game.py
@@ -6,6 +6,10 @@ while True:
     if guess == number: 
         print("You win!") 
     else: 
+        if guess < number: 
+            print("Too low!") 
+        elif guess > number: 
+            print("Too high!") 
         print(f"Wrong! The number was {number}") 
         print("Play again? (y/n)") 
         if input().lower() != 'y': 
